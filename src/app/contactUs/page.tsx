b4da6d6c3829b27ef1ef6d12175dd610d4e612be
indianerp01 <indianerp01@gmail.com>
2024-08-10 20:57:38 +0530

    Delete src/app/contactUs directory

--- src/app/contactUs/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function contactUs() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-        Contact Us Section Here
-      </main>
-      
-    );
-  }
