b000535627f33a512d22b7f92c74add9071aedf4
SanketPathare <sanketpathare8808@gmail.com>
2024-08-14 20:53:50 +0530

    pharmacy-1

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -3,11 +3,9 @@ import ShowCase from "../components/Showcase/ShowCase";
 import Footer from "../components/Footer/Footer"
 import FAQSection from "../components/FAQSection/FAQSection";
 import ProductsFeatures from "../components/ProductsFeatures/ProductsFeatures"
-// import Software from "../components/Software/Software";
 const page = () => {
   return <div>
     <ShowCase />
-    {/* <Software/>  */}
     <ProductsFeatures />
     <FAQSection />
     <Footer />
