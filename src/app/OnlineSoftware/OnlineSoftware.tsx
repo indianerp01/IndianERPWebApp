b4ea76f67857835b2a6ac002a79931a741785d8f
SanketPathare <sanketpathare8808@gmail.com>
2024-08-28 19:52:37 +0530

    all

--- src/app/OnlineSoftware/OnlineSoftware.tsx
+++ src/app/OnlineSoftware/OnlineSoftware.tsx
@@ -19,6 +19,14 @@ const OnlineSoftware = () => {
           {/* Button */}
           <button
             className={`w-1/2 py-4  text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none ${
+              activeTab === "all" ? "active:bg-gray-200" : ""
+            }`}
+            onClick={(event) => handleTabClick(event, "all")}
+          >
+          All Software
+          </button>
+          <button
+            className={`w-1/2 py-4  text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none ${
               activeTab === "tab1" ? "active:bg-gray-200" : ""
             }`}
             onClick={(event) => handleTabClick(event, "tab1")}
@@ -51,6 +59,29 @@ const OnlineSoftware = () => {
           </button>
         </div>
         {/* Tab Content */}
+        <div
+          id="all"
+          className={`tabcontent p-4 ${activeTab !== "all" ? "hidden" : ""}`}
+        >
+              {/* {all softwares} */}
+          <h2 className="text-3xl font-bold text-gray-800 m-7 text-center">
+            Retail Softwares
+          </h2>
+          <RetailSoftware />
+          <h2 className="text-3xl font-bold text-gray-800 m-7 text-center">
+          Distributor Software
+          </h2>
+          <DistributorSoftware/>
+          <h2 className="text-3xl font-bold text-gray-800 m-7 text-center">
+          Manufacturing Software
+          </h2>
+          <ManufacturingSoftware/>
+          <h2 className="text-3xl font-bold text-gray-800 m-7 text-center">
+          ERP Corporate Solutions
+          </h2>
+          <ERPCorporateSolutions/>
+        </div>
+        {/* {all softwares} */}
         <div
           id="tab1"
           className={`tabcontent p-4 ${activeTab !== "tab1" ? "hidden" : ""}`}
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,91 +1,167 @@
-import React from "react";
-import Link from "next/link";
+import { FaFacebook } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+
 const Footer = () => {
   return (
-    <div>
-      <div className="bg-gray-100">
-        <div className="max-w-screen-lg px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
-          <div className="p-5">
-            <span className="flex items-center justify-center font-bold">
-              <span className="text-orange-500 text-3xl">I-</span>
-              <span className="text-blue-500 text-3xl">C-</span>
-              <span className="text-green-500 text-3xl">TECH</span>
-            </span>
-          </div>
-          <div className="p-5">
-            <div className="text-sm uppercase text-orange-600 font-bold">
-              Useful Links
-            </div>
-            <Link className="my-3 block" href="/#">
-              Pharmacy software
-              <span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-            <Link className="my-3 block" href="/#">
-              Super Market Software
-              <span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-            <Link className="my-3 block" href="/#">
-              Jewellery Software
-              <span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-            <Link className="my-3 block" href="/#">
-              Restaurant Software
-              <span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-            <Link className="my-3 block" href="/#">
-              Garment Software
-              <span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-            <Link className="my-3 block" href="/#">
-              Retail Software<span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-          </div>
-          <div className="p-5">
-            <div className="text-sm uppercase text-orange-600 font-bold">
-              Support
-            </div>
-            <Link className="my-3 block" href="/#">
-              Help Center <span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-            <Link className="my-3 block" href="/#">
-              Privacy Policy <span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-            <Link className="my-3 block" href="/#">
-              Conditions <span className="text-teal-600 text-xs p-1"></span>
-            </Link>
-          </div>
-          <div className="p-5">
-            <div className="text-sm uppercase text-orange-600 font-bold">
-              Contact us
-            </div>
-            <p className="my-3 block">
-              {" "}
-              Address Indian Computer Technology : Office No.1, Sr.No.99/12B/2,
-              Vijay Nagar Alfanso Residency, Near Mother Teresa Fly Over,
-              Kalewadi, Pimpri, Pimpri-Chinchwad, Pune-411017
-              <span className="text-teal-600 text-xs p-1"></span>
-            </p>
-            <p className="my-3 block">
-              indiancomptech@gmail.com{" "}
-              <span className="text-teal-600 text-xs p-1"></span>
-            </p>
-          </div>
-        </div>
-      </div>
+    <>
+      <footer className="bg-[#0c0b0b11] ">
+        <div className="container mx-auto p-0 md:p-8 xl:px-0 ">
+          <div className="mx-auto max-w-7xl px-6  pt-3">
+            <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+              <div className="space-y-4">
+                <div>
+                  <a href="/">
+                    <div className="flex items-center space-x-2 text-2xl font-medium">
+                      <span className="flex items-center justify-center font-bold">
+                        <span className="text-orange-500 text-2xl">I-</span>
+                        <span className="text-blue-500 text-2xl">C-</span>
+                        <span className="text-green-500 text-2xl">TECH</span>
+                      </span>
+                    </div>
+                  </a>
+                </div>
+                <div className="max-w-md pr-16 text-md0">
+                  Enhance productivity and efficiency with cutting-edge
+                  artificial intelligence solutions for your business
+                  operations.
+                </div>
+                <div className="flex space-x-2 text-2xl">
+                  <a href="" target="_blank">
+                    <FaFacebook  />
+                  </a>
+                  <a href="" target="_blank">
+                    <FaSquareXTwitter/>
+                  </a>
+                  <a href="" target="_blank">
+                    <FaLinkedin />
+                  </a>
+                  <a href="" target="_blank">
+                    <FaSquareInstagram />
+                  </a>
 
-      <div className="bg-gray-100 pt-2">
-        <div className="flex pb-5 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col max-w-screen-lg items-center">
-          <div className="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
-            <Link href="/#" className="w-6 mx-1"></Link>
-            <Link href="/#" className="w-6 mx-1"></Link>
-            <Link href="/#" className="w-6 mx-1"></Link>
-            <Link href="/#" className="w-6 mx-1"></Link>
-            <Link href="/#" className="w-6 mx-1"></Link>
+                </div>
+              </div>
+              <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+                <div className="md:grid md:grid-cols-2 md:gap-8">
+                  <div>
+                    <h3 className="text-md font-semibold leading-6 ">
+                      Useful Links
+                    </h3>
+                    <ul role="list" className="mt-6 space-y-4">
+                      <li>
+                        <a href="/" className="text-md leading-6">
+                          Home
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/Retail" className="text-md leading-6">
+                          Retail
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/Distribution"
+                          className="text-md leading-6"
+                        >
+                          Distribution
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/Manufacturing"
+                          className="text-md leading-6"
+                        >
+                          Manufacturing
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/ERP"
+                          className="text-md leading-6"
+                        >
+                          ERP
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="mt-10 md:mt-0">
+                    <h3 className="text-md font-semibold leading-6 ">
+                      Use Software
+                    </h3>
+                    <ul role="list" className="mt-6 space-y-4">
+                      <li>
+                        <a
+                          href="/MobileApps"
+                          className="text-md leading-6 "
+                        >
+                          Mobile Apps
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/Others"
+                          className="text-md leading-6 "
+                        >
+                          Others
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/OnlineSoftware" className="text-md leading-6 ">
+                          Online Software
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="md:grid md:grid-cols-2 md:gap-8">
+                  <div>
+                    <h3 className="text-md font-semibold leading-6 ">
+                      Resources
+                    </h3>
+                    <ul role="list" className="mt-6 space-y-4">
+                      <li>
+                        <a href="/aboutus" className="text-md leading-6 ">
+                          About Us
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/Contact" className="text-md leading-6 ">
+                          Contact Us
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="mt-10 md:mt-0">
+                    <h3 className="text-md font-semibold leading-6">
+                      Contact Details
+                    </h3>
+                    <h4 className="mt-6 space-y-4">Email id :</h4>
+                    <p>indiancomptech@gmail.com</p>
+
+                    <h4 className="mt-6 space-y-4">Contact Number :</h4>
+                    <p>
+                      
+                      +91 9921163448 , <br /> +91 9922163448
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-16 border-t border-gray-400/30 pt-8 sm:mt-20 lg:mt-6">
+              <div className="text-md text-center  ">
+                Copyright © 2024 .
+                <a rel="noopener" href="/">
+                  {" "}
+                  ICT
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="my-5">© Copyright 2020. All Rights Reserved.</div>
         </div>
-      </div>
-    </div>
+      </footer>
+    </>
   );
 };
 
