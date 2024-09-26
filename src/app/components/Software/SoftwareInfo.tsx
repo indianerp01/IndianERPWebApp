87468492e6b2e9d5d11fef3a060d5f53274cb3f4
GauravJawalkar <gauravjawalkar8@gmail.com>
2024-09-26 19:13:56 +0530

    grayscale opt

--- src/app/components/Software/SoftwareInfo.tsx
+++ src/app/components/Software/SoftwareInfo.tsx
@@ -7,7 +7,7 @@ const SoftwareInfo = ({ src, name }: SoftwareInfoProp) => {
         <img
           src={src}
           alt=""
-          className="w-full h-[300px] object-contain object-center grayscale-[20%]"
+          className="w-full h-[300px] object-contain object-center "
         />
       </div>
       <button className="bg-gray-100 hover:bg-gray-200 transition-all ease-linear duration-300 px-4 py-2 rounded-sm text-center">
